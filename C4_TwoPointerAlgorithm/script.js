a09dafc771d42c3f4486d94957e3597c94458650
Miguel Ortega <miguelangelortegavela5@gmail.com>
2026-08-01 23:46:20 -0600

    Two Pointers Algorithm practise finished.

--- C4_TwoPointerAlgorithm/script.js
+++ C4_TwoPointerAlgorithm/script.js
@@ -2,3 +2,16 @@
 let num = [15, 20, 14, 9, 27, 25, 24, 47, 16, 85, 26];
 let res = num.filter((num) => num >= 20 && num <= 30);
 console.log(res);
+
+// GuestsFinder.js
+let Guests = ['Ana', 'Carlos', 'Cecilia', 'Daniel', 'Diana', 'Eduardo'];
+
+function findPair(arr) {
+  let start = 0;
+  let next = 1;
+
+  while (next < arr.length) {}
+  return null;
+}
+
+console.log(findPair(Guests));
